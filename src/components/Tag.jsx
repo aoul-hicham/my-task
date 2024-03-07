d663868b249acccee3d26c3cb09802145249b3ea
Hicham elaoula <aoul.hicham@gmail.com>
2024-03-07 09:29:44 +0100

    mention the selected task

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -1,7 +1,23 @@
 import "./Tag.css"
 
-const Tag = ({ tagName }) => {
-  return <button className="tag">{tagName}</button>
+const Tag = ({ tagName, selectTag, isSelected }) => {
+  const tagStyles = {
+    HTML: { backgroundColor: "#fda821" },
+    CSS: { backgroundColor: "#15d4c8" },
+    JavaScript: { backgroundColor: "#ffd12c" },
+    React: { backgroundColor: "#4cdafc" },
+    Default: { backgroundColor: "#f9f9f9" },
+  }
+  return (
+    <button
+      type="submit"
+      className="tag"
+      style={isSelected ? tagStyles[tagName] : tagStyles["Default"]}
+      onClick={() => selectTag(tagName)}
+    >
+      {tagName}
+    </button>
+  )
 }
 
 export default Tag
--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,23 +1,78 @@
+import { useState } from "react"
 import Tag from "./Tag"
 
 import "./TaskForm.css"
 
 const TaskForm = () => {
+  const [taskForm, setTaskForm] = useState({
+    task: null,
+    status: "todo",
+    tags: [],
+  })
+
+  //* Handling input change
+  const handlingInputchange = (e) => {
+    const { name, value } = e.target
+
+    setTaskForm((data) => {
+      return { ...data, [name]: value }
+    })
+  }
+
+  //* Check if tag is selected
+  const tagIsSelected = (tag) => {
+    return taskForm.tags.includes(tag)
+  }
+
+  //* Handling tag selection
+  const selectTag = (tag) => {
+    const tagAlreadySelected = tagIsSelected(tag)
+
+    if (tagAlreadySelected) {
+      const filteredTags = taskForm.tags.filter((tagV) => tagV != tag)
+
+      setTaskForm((data) => {
+        return { ...data, tags: filteredTags }
+      })
+    } else {
+      setTaskForm((data) => {
+        return { ...data, tags: [...data.tags, tag] }
+      })
+    }
+  }
+
+  console.log(taskForm.tags)
+
+  //* Handling form submit
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
     <>
       <header className="app_header">
-        <form>
-          <input type="text" className="task_input" placeholder="Enter your task" />
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="task"
+            className="task_input"
+            placeholder="Enter your task"
+            onChange={handlingInputchange}
+          />
           <div className="task_form_bottom_line">
             <div>
-              <Tag tagName="HTML" />
-              <Tag tagName="CSS" />
-              <Tag tagName="JavaScript" />
-              <Tag tagName="React" />
+              <Tag tagName="HTML" selectTag={selectTag} isSelected={tagIsSelected("HTML")} />
+              <Tag tagName="CSS" selectTag={selectTag} isSelected={tagIsSelected("CSS")} />
+              <Tag
+                tagName="JavaScript"
+                selectTag={selectTag}
+                isSelected={tagIsSelected("JavaScript")}
+              />
+              <Tag tagName="React" selectTag={selectTag} isSelected={tagIsSelected("React")} />
             </div>
 
             <div>
-              <select name="" id="" className="task_status">
+              <select name="status" id="" className="task_status" onChange={handlingInputchange}>
                 <option value="todo">To do</option>
                 <option value="doing">Doing</option>
                 <option value="done">Done</option>
